b211f01b150a78feff4b7457501f443fc506f113
Brotati katha <brotatihalderkatha@gmail.com>
2021-08-07 16:18:27 +0600

    logout features added

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,40 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Header() {
+  const user = JSON.parse(localStorage.getItem("user-info"));
+  const history = useHistory();
+
+  function logout() {
+    localStorage.clear();
+    history.push("/register");
+  }
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto nav-bar-wrapper">
-          {localStorage.getItem("user-info") ? 
+          {localStorage.getItem("user-info") ? (
             <>
               <Link to="/add">Add Product</Link>
               <Link to="/update">Update Product</Link>
             </>
-           : 
+          ) : (
             <>
               <Link to="/login">login</Link>
               <Link to="/register">Register</Link>
             </>
-          }
+          )}
         </Nav>
+        {localStorage.getItem("user-info") ? 
+          <Nav>
+            <NavDropdown title={user && user.name}>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+              <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+         : null}
       </Navbar>
     </div>
   );
